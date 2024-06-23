824c0cb21c60cdedd3871f4ee8694528676c7496
timothyAgevi <timothyagevi@gmail.com>
2024-06-24 00:36:16 +0300

    add route to create user

---
+++ src/routes/userRoutes.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import { body } from 'express-validator';
+import { createUser } from '../controllers/userController';
+
+const router = Router();
+
+router.post('/user', [
+  body('name').notEmpty().withMessage('Name is required'),
+  body('idNumber').notEmpty().withMessage('ID number is required'),
+  body('phoneNumber').notEmpty().withMessage('Phone number is required'),
+  body('email').optional().isEmail().withMessage('Email must be valid'),
+], createUser);
+
+export default router;
